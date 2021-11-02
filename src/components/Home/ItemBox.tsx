efd875303fcbc32d83dba556846d9de69fc216a1
bao hoang <hoanggiabao15899@gmail.com>
2021-11-02 09:35:48 +0700

    add loading image

--- src/components/Home/ItemBox.tsx
+++ src/components/Home/ItemBox.tsx
@@ -1,16 +1,21 @@
 import { UrlImage } from 'api/Urls';
 import { IdataResults } from 'global/Home/Interfaces'
-import React from 'react'
+import React, { useState } from 'react'
 import LazyLoad from 'react-lazyload'
 import DefaultPoster from 'assets/image/defaultPoster.jpg'
-
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 export default function ItemBox(props: { data?: IdataResults, type: string }) {
+    const [loading, setLoading] = useState<Boolean>(false)
+
     return (
         <div className='itemBox'>
-            <LazyLoad height={220} once>
-                <img className='itemBox_img'
-                    src={props.data?.backdrop_path ? `${UrlImage.POSTER}${props.data?.backdrop_path}` : DefaultPoster} />
-            </LazyLoad>
+            {!loading &&
+                <SkeletonTheme  baseColor="#202020" highlightColor="#444">
+                    <Skeleton className='skeletonLoad' />
+                </SkeletonTheme>}
+            <img onLoad={() => setLoading(!loading)} className='itemBox_img'
+                src={props.data?.backdrop_path ? `${UrlImage.POSTER}${props.data?.backdrop_path}` : DefaultPoster} />
             <p className='itemBox_title'>{props.data?.original_title ? props.data?.original_title : props.data?.original_name}</p>
         </div>
     )
